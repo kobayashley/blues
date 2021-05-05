6ab9f183982875a6c9d935016fc7eee7efeaff40
braxtonhall <braxtonjhall@gmail.com>
2021-05-04 19:38:38 -0700

    #49 MentionBlues listener should ignore replies

--- src/listeners/impl/mentionBlues.ts
+++ src/listeners/impl/mentionBlues.ts
@@ -8,7 +8,7 @@ const mentionBot: Listener<"message"> = {
     name: "mentionBot",
     event: "message",
     procedure: (client: Client) => async (message: Message) => {
-        if (message.author.bot === false && message.mentions.has(client.user.id)) {
+        if (message.author.bot === false && !message.reference && message.mentions.has(client.user.id)) {
             Log.info(`${message.author.username} mentioned the bot`);
             const prefix = await PrefixController.getPrefix(getGuild(message));
             return message.channel.send(`Did someone mention me? Try \`${prefix}help\` instead!`);

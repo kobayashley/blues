cff9a6a34186ff0c2dabfc9cb715c0cd0d572e78
braxtonhall <braxtonjhall@gmail.com>
2021-05-07 20:44:55 -0700

    #5 Deployment preparation

--- src/server/routes/registerRoutes.ts
+++ src/server/routes/registerRoutes.ts
@@ -8,9 +8,9 @@ import {handleOAuth} from "./handleOAuth";
 const prefix = getConfig(ConfigKey.pathPrefix);
 
 const registerRoutes = (server: Express, client: Client): Express => {
-    server.get(getPath(ConfigKey.googleOAuthCallback), handleOAuth(client, readGoogleParams, makeYouTubePlaylist));
-    server.get(getPath(ConfigKey.spotifyOAuthCallback), handleOAuth(client, readSpotifyParams, makeSpotifyPlaylist));
-    server.get("/", home);
+    server.get(prefix + getPath(ConfigKey.googleOAuthCallback), handleOAuth(client, readGoogleParams, makeYouTubePlaylist));
+    server.get(prefix + getPath(ConfigKey.spotifyOAuthCallback), handleOAuth(client, readSpotifyParams, makeSpotifyPlaylist));
+    server.get(prefix + "/", home);
     return server;
 };
 

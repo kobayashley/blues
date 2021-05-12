bb507a70d51f144568b8b1bca8fe6d67e3ae6687
braxtonhall <braxtonjhall@gmail.com>
2021-05-11 22:28:12 -0700

    #71 Make naming consistent with type

--- src/server/routes/handleOAuth.ts
+++ src/server/routes/handleOAuth.ts
@@ -10,21 +10,22 @@ import {PlatformController} from "../../controllers/platforms/PlatformController
 const database = getDatabaseAdapter();
 const prefix = getConfig(ConfigKey.pathPrefix);
 
-const handleOAuth = (client: Client, readParams: ReadParams, makePlaylist: MakePlatformController) => async (req: Request, res: Response): Promise<void> => {
-    try {
-        const {options, clientToken} = readParams(req);
-        if (await database.hasToken(options.token)) {
-            const platform = await makePlaylist(options, clientToken);
-            res.render("playlist", {...options, title: "playlist", prefix});
-            return new PlaylistController(platform).sendPlaylist(client, options);
-        } else {
-            throw new Error("Token is not valid");
+const handleOAuth = (client: Client, readParams: ReadParams, makeController: MakePlatformController) =>
+    async (req: Request, res: Response): Promise<void> => {
+        try {
+            const {options, clientToken} = readParams(req);
+            if (await database.hasToken(options.token)) {
+                const platform = await makeController(options, clientToken);
+                res.render("playlist", {...options, title: "playlist", prefix});
+                return new PlaylistController(platform).sendPlaylist(client, options);
+            } else {
+                throw new Error("Token is not valid");
+            }
+        } catch (err) {
+            Log.error("Problem handling OAuth", err);
+            res.render("error", {prefix});
         }
-    } catch (err) {
-        Log.error("Problem handling OAuth", err);
-        res.render("error", {prefix});
-    }
-};
+    };
 
 export type ReadParams = (req: Request) => {options: AuthorizationState, clientToken: string};
 export type MakePlatformController = (options: PlaylistOptions, clientToken: string) => Promise<PlatformController>;
